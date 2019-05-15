6b5bb84c73e6411ee6272befb1755be11c1b60c1
blacksabbather <jay_wong@me.com>
2019-05-15 15:44:02 +0800

    transfer address information

--- create_transaction.js
+++ create_transaction.js
@@ -7,9 +7,23 @@ module.exports =async function handleRequest(req, res) {
 			intent: 'CAPTURE',
 			purchase_units: [{
 					amount: {
-					currency_code: 'USD',
-					value: req.body.value
-					}
+							currency_code: 'USD',
+							value: req.body.value
+					},
+					shipping:{
+						method: "United States Postal Service",
+        				address: {
+          					name: {
+            					give_name:req.body.firstname,
+            					surname:req.body.lastname
+          					},
+							address_line_1: req.body.address,
+							admin_area_1: req.body.state,
+							admin_area_2: req.body.city,
+							postal_code: req.body.zipcode,
+							country_code: "US"
+        				}
+					},	
 			}],
 	});
 	let order;
@@ -20,6 +34,6 @@ module.exports =async function handleRequest(req, res) {
 		return res.send(500);
 	}
 	res.status(200).json({
-		orderID: order.result.id //send the order ID back
+		orderID: order.result.id
 	});
 }	
